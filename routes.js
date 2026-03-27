51b5b3dfd89cc21e0c4b2db5fd44423f9d2c727b
saidmtanzania <saidmtanzania@gmail.com>
2026-03-27 12:00:41 +0300

    add about us route with dynamic data loading functionality

--- routes.js
+++ routes.js
@@ -1,3 +1,27 @@
 module.exports = (app, renderLayout) => {
+    app.get("/about-us", (req, res) => {
+        const content = `
+            <h1>About Us</h1>
+            <button onclick="load()">Load Data</button>
+            <div id="result" style="margin-top:20px;"></div>
+            
+            <script>
+                async function load() {
+                    try {
+                        const res = await fetch('/api/about-us');
+                        const data = await res.json();
+
+                        document.getElementById("result").innerHTML =
+                        "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+
+                    } catch (err) {
+                        document.getElementById("result").innerText =
+                        "Error loading data";
+                    }
+                }
+            </script>
+        `;
+        res.send(renderLayout(content));
+    });
     
 };
